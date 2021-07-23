0bff7ac8de59d763d9fb217fb4ba2ac7084ce577
Enoch <enoch@getinnotized.com>
2021-07-23 16:03:57 +0000

    logs fallback full directory

--- src/app.ts
+++ src/app.ts
@@ -23,6 +23,7 @@ app.use(express.static(path.join(__dirname, "../public")));
 
 
 let fallpath = path.join(__dirname, "../public/fe/index.html");
+console.log('fallback', fallpath);
 app.get("**", (req: Request, res: Response) => {
   res.sendFile(fallpath);
 });
