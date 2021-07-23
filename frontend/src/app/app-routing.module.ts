2158637a290d40271fb81b37c9e460f795230a52
Enoch <enoch@getinnotized.com>
2021-07-23 15:33:12 +0000

    serves frontend files

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { LayoutComponent } from './components/shared/layout/layout.component';
 import { SignupComponent } from './components/signup/signup.component';
 
 const routes: Routes = [
+  {path: "", redirectTo: "login", pathMatch: "full"},
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'home', component: LayoutComponent },
