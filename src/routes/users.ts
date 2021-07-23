f31ea16b794a5c29e4730bcf9b216851a4b7f114
Enoch <enoch@getinnotized.com>
2021-07-23 14:17:43 +0000

    updates package to allow build of ts to js

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -5,7 +5,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req: Request, res: Response, next: NextFunction) {
-  res.send('respond with a resource');
+  res.status(201).json({message: 'respond with a resource'});
 });
 
 module.exports = router;
