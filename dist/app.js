959ca0d5a2ff0165fffdea2e88d5cef0c94d80dd
Enoch <enoch@getinnotized.com>
2021-07-23 13:44:23 +0000

    app.js updated

--- dist/app.js
+++ dist/app.js
@@ -19,9 +19,9 @@ app.use(logger('dev'));
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express_1.default.static(path.join(__dirname, "./")));
+app.use(express_1.default.static(path.join(__dirname, "../dist")));
 app.get("**", (req, res) => {
-    res.sendFile(path.join(__dirname, "./fe/index.html"));
+    res.sendFile(path.join(__dirname, "../dist/fe/index.html"));
 });
 app.use('/users', usersRouter);
 // catch 404 and forward to error handler
