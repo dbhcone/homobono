5a65c4ec3dd4c674960a3e3c1480e5bb4965a636
Enoch <enoch@getinnotized.com>
2021-08-30 15:37:52 +0000

    wip: header user data setup

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  username = null;
+  constructor(private auth: AuthService) {
+    // this.username = this.auth.session().username;
+   }
 
   ngOnInit(): void {
   }
