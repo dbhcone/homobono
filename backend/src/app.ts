bbd84388cbe4605abda1254778eb04481c1e0e97
Enoch <enoch@getinnotized.com>
2021-09-02 00:20:30 +0000

    static files path modified

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -31,7 +31,7 @@ app.use('/api/user', userRouter);
 app.use('/api/events', eventRouter);
 
 // serve only the static files from the dist directory
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../public/fe')));
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/fe/'));
