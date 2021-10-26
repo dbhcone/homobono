8dec5a432d0c11586674ea1aaf64c0b46542b326
Enoch <enoch@getinnotized.com>
2021-10-26 12:46:33 +0000

    default user role fixed

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -5,7 +5,7 @@ const UserSchema: Schema = new Schema({
     email: {type: String, required: true, unique: true},
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, required: true, default: 'member' },
+    role: { type: String, required: true, default: 'user' },
     accountOwner: { type: Schema.Types.ObjectId, required: true, unique: true, ref: 'Accounts' },
     status: {type: String, required: true, default: 'inactive'},
     profilePhoto: { type: Object },
